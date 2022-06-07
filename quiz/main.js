f634ee41fddc6e6c9c2327960f90f607415be931
tmcoco0228si <proguteinitar3150@gmail.com>
2022-06-08 00:10:25 +0900

    edit

--- quiz/main.js
+++ quiz/main.js
@@ -12,8 +12,7 @@
   ];
   let currentNum = 0;
 
-  question.textContent = quizSet[currentNum].q;
-
+  //問題シャッフル
   function shuffle(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -22,12 +21,42 @@
     return arr;
   }
 
-  const shuffledChoices = shuffle([...quizSet[currentNum].c]);
- 
-  // console.log(quizSet[currentNum].c);
-  shuffledChoices.forEach((choice) => {
-    const li = document.createElement("li");
-    li.textContent = choice;
-    choices.appendChild(li);
+  //正誤判定
+  function checkAnswer(li) {
+    if (li.textContent === quizSet[currentNum].c[0]) {
+      li.classList.add("correct");
+    } else {
+      li.classList.add("wrong");
+    }
+
+    btn.classList.remove("disabled");
+  }
+
+  function setQuiz() {
+    question.textContent = quizSet[currentNum].q;
+
+    while (choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
+
+    //スプレット構文にすることで、関数の引数にオブジェクトを渡した際には、オブジェクトの参照を渡すのではなく、値を渡すようにしている。
+    const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+
+    // console.log(quizSet[currentNum].c);
+    shuffledChoices.forEach((choice) => {
+      const li = document.createElement("li");
+      li.textContent = choice;
+      li.addEventListener("click", () => {
+        checkAnswer(li);
+      });
+      choices.appendChild(li);
+    });
+  }
+
+  setQuiz();
+
+  btn.addEventListener("click", () => {
+    currentNum++;
+    setQuiz();
   });
 }
